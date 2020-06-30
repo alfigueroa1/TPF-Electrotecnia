0ae7d1854219fc59aa288f9a6a5feb1e0981d92d
Facundo <famolina@itba.edu.ar>
2020-06-30 17:28:14 -0300

    SignalResponse: graficos Sine and Step input. Distribution: zeros and poles on S plane

--- FunctionGraphing/Distribution.py
+++ FunctionGraphing/Distribution.py
@@ -3,17 +3,21 @@ import Config
 from tkinter import *
 from UserInput import userInput
 
+from collections import defaultdict
+
 import scipy.signal as ss
-from numpy import linspace, logspace, cos, abs, pi
+import numpy as np
+from numpy import linspace, logspace, cos, abs, pi, roots, amax, concatenate, vstack, rint
 import matplotlib.pyplot as plt
+import matplotlib.transforms 
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
-import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from matplotlib.patches import Circle
+
 from scipy import signal
-from numpy import pi
 
 
 class Distribution(tk.Frame):
@@ -34,6 +38,20 @@ class Distribution(tk.Frame):
 
         self.H = ss.TransferFunction([1], [1])
         self.getTransf()
+
+
+        self.f = Figure(figsize=(5, 3), dpi=100)
+
+        self.ax1 = self.f.add_subplot(111)
+
+        self.canvas = FigureCanvasTkAgg(self.f, self)
+        #canvas.show()
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
+        self.toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
 
         self.backButton = tk.Button(
             self,
@@ -102,6 +120,72 @@ class Distribution(tk.Frame):
         #########
         ##ACA HACER COSAS CON GRAFICOS EN BASE A H
         #########
+
+        self.ax1.clear()
+            
+        z = roots(Vout)
+        p = roots(Vin)
+
+        r = 1.5 * amax(concatenate((abs(z), abs(p), [1])))
+        self.ax1.axis('scaled')
+        self.ax1.axis([-r, r, -r, r])
+        
+        # Plot the poles and set marker properties
+        self.ax1.plot(p.real, p.imag, 'x', markersize=9, alpha=0.5, label = 'Poles', color = 'b')
+    
+        # Plot the zeros and set marker properties
+        self.ax1.plot(z.real, z.imag,  'o', markersize=9, alpha=0.5, label = 'Zeros', color = 'g')
+        
+        #self.ax1.add_patch(unit_circle)
+
+        self.ax1.axvline(0, color='0.4')
+        self.ax1.axhline(0, color='0.4')
+        self.ax1.minorticks_on()
+        self.ax1.grid(which='major', color='black', linewidth=0.8, linestyle='--')
+        self.ax1.grid(which='minor', color='black', linewidth=0.4, linestyle=':')
+        # Sets figure data.
+        self.ax1.set_title('S-plane')
+        self.ax1.set_xlabel('\u03C3')
+        self.ax1.set_ylabel('j2πf')
+        self.ax1.legend()
+
+        #self.ax1.xaxis.set_label_coords(1.05, -0.025)
+        """
+        If there are multiple poles or zeros at the same point, put a 
+        superscript next to them.
+        TODO: can this be made to self-update when zoomed?
+
+        # Finding duplicates by same pixel coordinates (hacky for now):
+        poles_xy = self.ax1.transData.transform(vstack(p[0].get_data()).T)
+        zeros_xy = self.ax1.transData.transform(vstack(z[0].get_data()).T)    
+
+        # dict keys should be ints for matching, but coords should be floats for 
+        # keeping location of text accurate while zooming
+
+        # TODO make less hacky, reduce duplication of code
+        d = defaultdict(int)
+        coords = defaultdict(tuple)
+        for xy in poles_xy:
+            key = tuple(np.rint(xy).astype('int'))
+            d[key] += 1
+            coords[key] = xy
+        for key, value in d.iteritems():
+            if value > 1:
+                x, y = self.ax1.transData.inverted().transform(coords[key])
+                plt.text(x, y, r' ${}^{' + str(value) + '}$',fontsize=13)
+
+        d = defaultdict(int)
+        coords = defaultdict(tuple)
+        for xy in zeros_xy:
+            key = tuple(np.rint(xy).astype('int'))
+            d[key] += 1
+            coords[key] = xy
+        for key, value in d.iteritems():
+            if value > 1:
+                x,y = self.ax1.transData.inverted().transform(coords[key])
+                plt.text(x, y, r' ${}^{' + str(value) + '}$', fontsize=13)
+        """ 
+
         self.canvas.draw()
 
         pass
--- FunctionGraphing/SignalResponse.py
+++ FunctionGraphing/SignalResponse.py
@@ -4,7 +4,7 @@ from tkinter import *
 from UserInput import userInput
 
 import scipy.signal as ss
-from numpy import linspace, logspace, cos, abs, pi
+from numpy import linspace, logspace, cos, abs, pi, sin
 import matplotlib.pyplot as plt
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -12,8 +12,6 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
-from scipy import signal
-from numpy import pi
 
 
 class SignalResponse(tk.Frame):
@@ -34,6 +32,20 @@ class SignalResponse(tk.Frame):
 
         self.H = ss.TransferFunction([1], [1])
         self.getTransf()
+
+
+
+        self.f = Figure(figsize=(5, 3), dpi=100)
+
+        self.ax1 = self.f.add_subplot(111)
+
+        self.canvas = FigureCanvasTkAgg(self.f, self)
+        #canvas.show()
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self)
+        self.toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
         self.backButton = tk.Button(
@@ -58,6 +70,11 @@ class SignalResponse(tk.Frame):
     def focus(self):
         Vin = [1]
         Vout = [1]
+
+        xinput = userInput.get("inputFunction")
+        amp = userInput.get("inputAmplitude")
+        finput = userInput.get("inputFreq")
+
         order = userInput.get("order")
         filterType = userInput.get("type")
         f0 = userInput.get("frequency")
@@ -95,14 +112,47 @@ class SignalResponse(tk.Frame):
                 Vout = k * [pow(1 / f0, 2), 0, 1]
                 Vin = [pow(1 / f0, 2), 2 * epsilon / f0, 1]
 
+
         self.H = ss.TransferFunction(Vout, Vin)
 
-        x2 = logspace(-1, 5, num=1000)
-        Bode = ss.bode(self.H, x2)  # Bode diagram.
+        self.ax1.clear()
 
-        #########
-        ##ACA HACER COSAS CON GRAFICOS EN BASE A H
-        #########
+        if xinput == "Sine":
+            
+            t = linspace(0, 6, num = 1000)
+            u = amp*sin(2*pi*finput*t)
+            tout, yout, xout = ss.lsim(self.H, U = u, T = t)
+            
+            #self.ax1.set_xscale('log')
+            self.ax1.plot(t, u, color = 'r', linewidth=0.5, label = 'input')
+            self.ax1.plot(tout, yout, color = 'b', linewidth=0.5, label = 'output')
+            self.ax1.minorticks_on()
+            self.ax1.grid(which='major', color='black', linewidth=0.8, linestyle='--')
+            self.ax1.grid(which='minor', color='black', linewidth=0.4, linestyle=':')
+            # Sets figure data.
+            self.ax1.set_title('Sinusoidal input signal')
+            self.ax1.set_xlabel('Time [s]')
+            self.ax1.set_ylabel('Amplitude [V]')
+            self.ax1.legend()
+
+        elif xinput == "Step":
+
+            t,Step = ss.step(self.H)
+            
+
+            self.ax1.plot(t, amp * Step, color = 'b')
+            self.ax1.minorticks_on()
+            self.ax1.grid(which='major', color='black', linewidth=0.8, linestyle='--')
+            self.ax1.grid(which='minor', color='black', linewidth=0.4, linestyle=':')
+            # Sets figure data.
+            self.ax1.set_title('A*u(t) input signal')
+            self.ax1.set_xlabel('Time [s]')
+            self.ax1.set_ylabel('Amplitude [V]')
+
+
+
+
+
         self.canvas.draw()
 
         pass
